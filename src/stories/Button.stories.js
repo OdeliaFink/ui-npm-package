70d00e39f323abe232035e752317bee7c05d47e5
OdeliaFink <odelia.finkel@gmail.com>
2023-08-14 10:11:26 -0400

    added stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,7 +1,7 @@
 import Button from '../components/Button';
 
 export default {
-  title: 'Components/Button',
+  title: 'components/Button',
   component: Button,
   argTypes: { handleClick: { action: 'handleClick' } },
 };
@@ -11,34 +11,24 @@ const Template = (args) => <Button {...args} />;
 export const Red = Template.bind({});
 Red.args = {
   backgroundColor: 'red',
-  label: 'Press Me',
+  label: 'press me',
   size: 'md',
 };
-
 export const Green = Template.bind({});
 Green.args = {
   backgroundColor: 'green',
-  label: 'Press Me',
+  label: 'press me',
   size: 'md',
 };
-
 export const Small = Template.bind({});
 Small.args = {
   backgroundColor: 'red',
-  label: 'Press Me',
+  label: 'press me',
   size: 'sm',
 };
-
 export const Large = Template.bind({});
 Large.args = {
   backgroundColor: 'red',
-  label: 'Press Me',
+  label: 'press me',
   size: 'lg',
-};
-
-export const LongLabel = Template.bind({});
-LongLabel.args = {
-  backgroundColor: 'red',
-  label: 'Press Me adsf asdf asdf asdfasdfasd fasd fasd fasd',
-  size: 'md',
 };
